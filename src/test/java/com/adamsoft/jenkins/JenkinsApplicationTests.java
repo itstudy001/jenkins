8f8272e4aba5cb28be5f3acd866427379a213d8f
itggangpae <ggangpae1@gmail.com>
2024-11-10 07:43:20 +0900

    checkout

---
+++ src/test/java/com/adamsoft/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamsoft.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
